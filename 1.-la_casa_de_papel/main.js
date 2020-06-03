c3e56975b50c03ac7d6d697e3114a4fc9bb797e0
isabelkm77 <isabel.saa@km77.com>
2020-06-03 20:31:01 +0200

    Update first version La casa de papel challenge

---
+++ 1.-la_casa_de_papel/main.js
@@ -0,0 +1,7 @@
+var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+
+if (capital ==='Berlín' || capital ==='Tokio' || capital ==='Moscú'){
+    alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
+}else{
+    alert('No es un nombre de capital de uno de los miembros de mi equipo');
+}
