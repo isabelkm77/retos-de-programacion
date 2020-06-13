c0195cbaf8963d92764c881cac8e2cc31d202ed7
isabelkm77 <isabel.saa@km77.com>
2020-06-13 13:48:57 +0200

    Update code

--- 1.-la_casa_de_papel/main.js
+++ 1.-la_casa_de_papel/main.js
@@ -6,19 +6,49 @@
 //     alert('No es un nombre de capital de uno de los miembros de mi equipo');
 // }
 
+// var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+
+// if (checkCapital(capital))
+// {
+//     alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
+// }
+// else{
+//     alert('No es un nombre de capital de uno de los miembros de mi equipo');
+// }
+
+// function checkCapital (capital){
+//     if (capital ==='Berlín' || capital ==='Tokio' || capital ==='Moscú'){
+
+//         return true;
+//     }
+// }
+
+// var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+
+// switch(checkCapital(capital)){
+//     case true:
+//             alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
+//             break;
+//     default:
+//             alert('No es un nombre de capital de uno de los miembros de mi equipo');
+//             break;
+// }
+
+// function checkCapital (capital){
+//     if (capital === 'Berlín' || capital === 'Tokio' || capital === 'Moscú'){
+
+//         return true;
+//     }
+// }
+
+
 var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
-
-if (checkCapital(capital))
-{
-    alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
-}
-else{
-    alert('No es un nombre de capital de uno de los miembros de mi equipo');
-}
-
-function checkCapital (capital){
-    if (capital ==='Berlín' || capital ==='Tokio' || capital ==='Moscú'){
-        return true;
+var capitalesNuestras = ["Berlín", "Tokio", "Moscú"];
+capitalesNuestras.forEach(function (elemento, indice, array) {
+    if (capital.includes(capitalesNuestras)) {
+        alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
     }
-}
-
+    else {
+        alert('No es un nombre de capital de uno de los miembros de mi equipo');
+    }
+});
