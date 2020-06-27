af06d5615e9d4b8b53ef652b32028ceafb0360d0
isabelkm77 <isabel.saa@km77.com>
2020-06-27 12:50:26 +0200

    Update code

--- 1.-la_casa_de_papel/1.-basic_code/main.js
+++ 1.-la_casa_de_papel/1.-basic_code/main.js
@@ -1,54 +1,53 @@
-// var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
 
-// if (capital ==='Berlín' || capital ==='Tokio' || capital ==='Moscú'){
-//     alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
-// }else{
-//     alert('No es un nombre de capital de uno de los miembros de mi equipo');
-// }
 
-// var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+if ('Berlín' === capital || capital === 'Tokio' || capital === 'Moscú') {
+    alert('Si ' + capital + ' es una capital y es miembro de nuestro equipo');
+} else {
+    alert('No es un nombre de capital de uno de los miembros de mi equipo');
+}
 
-// if (checkCapital(capital))
-// {
-//     alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
-// }
-// else{
-//     alert('No es un nombre de capital de uno de los miembros de mi equipo');
-// }
+var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
 
-// function checkCapital (capital){
-//     if (capital ==='Berlín' || capital ==='Tokio' || capital ==='Moscú'){
+if (checkCapital(capital)) {
+    alert('Si ' + capital + ' es una capital y es miembro de nuestro equipo');
+} else {
+    alert('No es un nombre de capital de uno de los miembros de mi equipo');
+}
 
-//         return true;
-//     }
-// }
+function checkCapital(capital) {
+    if (capital === 'Berlín' || capital === 'Tokio' || capital === 'Moscú') {
 
-// var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
+        return true;
+    }
+}
 
-// switch(checkCapital(capital)){
-//     case true:
-//             alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
-//             break;
-//     default:
-//             alert('No es un nombre de capital de uno de los miembros de mi equipo');
-//             break;
-// }
+var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
 
-// function checkCapital (capital){
-//     if (capital === 'Berlín' || capital === 'Tokio' || capital === 'Moscú'){
 
-//         return true;
-//     }
-// }
+switch (checkCapital(capital)) {
+    case true:
+        alert('Si ' + capital + ' es una capital y es miembro de nuestro equipo');
+        break;
+    default:
+        alert('No es un nombre de capital de uno de los miembros de mi equipo');
+        break;
+}
+
+function checkCapital(capital) {
+    if (capital === 'Berlín' || capital === 'Tokio' || capital === 'Moscú') {
+
+        return true;
+    }
+}
 
 
 var capital = prompt("Introduce una capital y te diré si es uno de los miembros de mi equipo");
 var capitalesNuestras = ["Berlín", "Tokio", "Moscú"];
-capitalesNuestras.forEach(function (elemento, indice, array) {
+capitalesNuestras.forEach(function(elemento, indice, array) {
     if (capital.includes(capitalesNuestras)) {
-        alert('Si '+capital+' es una capital y es miembro de nuestro equipo');
-    }
-    else {
+        alert('Si ' + capital + ' es una capital y es miembro de nuestro equipo');
+    } else {
         alert('No es un nombre de capital de uno de los miembros de mi equipo');
     }
 });
